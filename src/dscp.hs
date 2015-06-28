262fd1397a04fd383c2628efd8017bd2b1c141e3
Dmitry Suzdalev <dimsuz@gmail.com>
2015-06-29 00:01:48 +0200

    Completed the part which figures out the files to copy
    Only copying itself is a todo

--- src/dscp.hs
+++ src/dscp.hs
@@ -6,6 +6,7 @@ import Filesystem.Path.CurrentOS
 import qualified Control.Foldl as Fold
 import Prelude hiding (FilePath)
 import Data.List (intersect)
+import Control.Monad (filterM)
 
 -- lists all possible and supported names of drawable directory names
 dirNames = [ "drawable-ldpi",
@@ -39,10 +40,28 @@ checkDirs (sourceRootDir, targetRootDir) = do
      (False:_)   -> Left $ "Source directory " ++ sourceRootDir ++ " does not exist"
      otherwise -> Right ( (decodeString sourceRootDir), (decodeString targetRootDir) )
 
+drawableFilename :: String -> FilePath
+drawableFilename name = decodeString $ name ++ ".png"
+
+-- a file with respective name
+fileListInRoot
+  :: String -- a drawable name (without extension)
+  -> FilePath -- a path to drawable root dir ('/res')
+  -> [FilePath] -- a list of drawable dir paths within a root dir to check (relative paths)
+  -> IO [FilePath] -- a list of existing files with the given name in all drawable dirs
+fileListInRoot name root dirs = do
+  filterM testfile (fileList root)
+    where fileList dir = map (\d -> dir </> d </> drawableFilename name) dirs
+
+getDestPath :: String -> FilePath -> FilePath -> FilePath
+getDestPath name root filename = root </> (dirname filename) </> (drawableFilename name)
+
 printActionSummary :: String -> FilePath -> FilePath -> [FilePath] -> IO ()
 printActionSummary name srcRoot dstRoot dirs = do
+  sourceFiles <- fileListInRoot name srcRoot dirs
+  let destFiles = map (getDestPath name dstRoot) sourceFiles
   putStrLn "Copying files:"
-
+  mapM_ (\(src,dst) -> putStrLn $ show src ++ " => " ++ show dst) (zip sourceFiles destFiles)
 
 main = do
   let name = drawableName
